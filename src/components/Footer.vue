dcaa8a372a5aa2c57af149acddda42c3a68e960f
francescogentile25 <francescogentile250893@gmail.com>
2023-06-09 02:49:44 +0200

    FEAT add home page FEAT add fontawesome FEAT add profile-picture

---
+++ src/components/Footer.vue
@@ -0,0 +1,34 @@
+<template>
+    <footer>
+        <div class="footer">
+            <div class="icon fs-3 d-flex gap-3 m-2">
+                <a href="">
+                    <font-awesome-icon icon="fa-brands fa-github" />
+                </a>
+                <a href="">
+                    <font-awesome-icon icon="fa-brands fa-linkedin-in" />
+                </a>
+            </div>
+        </div>
+    </footer>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+@use '../style/general.scss' as *;
+@use '../style/color.scss' as*;
+
+a {
+    color: $white;
+}
+
+.footer {
+    position: fixed;
+    bottom: 0;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -3,5 +3,11 @@ import './style.css'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap/dist/js/bootstrap.js'
 import App from './App.vue'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faGithub, faLinkedinIn } from '@fortawesome/free-brands-svg-icons'
+library.add(faGithub, faLinkedinIn)
 
-createApp(App).mount('#app')
+createApp(App)
+    .component('font-awesome-icon', FontAwesomeIcon)
+    .mount('#app')
