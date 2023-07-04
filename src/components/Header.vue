430b1c93f60e1c6bf80385848e5b21b313844757
francescogentile25 <francescogentile250893@gmail.com>
2023-07-04 20:58:07 +0200

    FEAT change color and add logo

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,7 +2,9 @@
     <nav class="navbar navbar-expand-lg bg-body-tertiary bg-dark border-bottom border-bottom-dark" data-bs-theme="dark"
         :class="{ 'navbar-hidden': !navbarVisible }">
         <div class="container-fluid">
-            <a class="navbar-brand" href="#">Navbar</a>
+            <a class="navbar-brand" href="#">
+                <img src="../assets/logo-franco.svg" alt="">
+            </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
                 aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
